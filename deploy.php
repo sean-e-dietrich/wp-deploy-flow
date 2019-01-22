22c7a278d88a2a08fd84f2b9e0835d45c44580ca
Sean Dietrich <sean@kanopistudios.com>
2019-01-21 23:19:48 -0800

    Updated plugin information

--- deploy.php
+++ deploy.php
@@ -1,14 +1,16 @@
 <?php
+
 /*
 Plugin Name: wp-deploy-flow
-Plugin URI: http://demental.info
+Plugin URI: https://github.com/sean-e-dietrich/wp-deploy-flow
 Description: A command-line task to deploy
 Version: 0.1
-Author: Arnaud Sellenet
-Author URI: http://demental.info
+Author: Sean Dietrich
+Author URI: https://github.com/sean-e-dietrich
 License: GPL2
 dependencies: wp-migrate-db
 */
+
 if (defined('WP_CLI') && WP_CLI) {
-  include 'lib/command.php';
+	include 'lib/command.php';
 }
