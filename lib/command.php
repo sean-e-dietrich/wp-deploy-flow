16b097dc6f368a6cf381d8b7aa8d31a44cbf0197
Sean Dietrich <sean@kanopistudios.com>
2019-01-21 22:56:38 -0800

    Fixed plugin file formatting

--- lib/command.php
+++ lib/command.php
@@ -1,4 +1,5 @@
 <?php
+
 WP_CLI::add_command( 'deploy', 'WP_Deploy_Flow_Command' );
 
 /**
@@ -11,89 +12,89 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 
 	protected static $_env;
 	/**
-   * Push local to remote, both system and database
-   *
-   * @synopsis <environment> [--dry-run]
-   */
+	 * Push local to remote, both system and database
+	 *
+	 * @synopsis <environment> [--dry-run]
+	 */
 	public function push( $args = array(), $flags = array() ) {
-    $this->_push_command('commands', $args, $flags);
+		$this->_push_command('commands', $args, $flags);
 	}
 
 	/**
-   * Push local to remote, only filesystem
-   *
-   * @synopsis <environment> [--dry-run]
-   */
+	 * Push local to remote, only filesystem
+	 *
+	 * @synopsis <environment> [--dry-run]
+	 */
 	public function push_files( $args = array(), $flags = array() ) {
-    $this->_push_command('commands_for_files', $args, $flags);
-  }
-
-	/**
-   * Pull local from remote, both system and database
-   *
-   * @synopsis <environment> [--dry-run]
-   */
-	public function pull( $args = array(), $flags = array() ) {
-    $this->_pull_command('commands', $args, $flags);
+		$this->_push_command('commands_for_files', $args, $flags);
 	}
 
 	/**
-   * Pull local from remote, only filesystem
-   *
-   * @synopsis <environment> [--dry-run]
-   */
-	public function pull_files( $args = array(), $flags = array() ) {
-    $this->_pull_command('commands_for_files', $args, $flags);
-  }
+	 * Pull local from remote, both system and database
+	 *
+	 * @synopsis <environment> [--dry-run]
+	 */
+	public function pull( $args = array(), $flags = array() ) {
+		$this->_pull_command('commands', $args, $flags);
+	}
 
-  protected function _push_command($command_name, $args, $flags)
-  {
-    $this->params = self::_prepare_and_extract( $args );
-    $this->flags = $flags;
-    extract($this->params);
+	/**
+	 * Pull local from remote, only filesystem
+	 *
+	 * @synopsis <environment> [--dry-run]
+	 */
+	public function pull_files( $args = array(), $flags = array() ) {
+		$this->_pull_command('commands_for_files', $args, $flags);
+	}
+
+	protected function _push_command($command_name, $args, $flags)
+	{
+		$this->params = self::_prepare_and_extract( $args );
+		$this->flags = $flags;
+		extract($this->params);
 
 		if ( $locked === true ) {
 			return WP_CLI::error( "$env environment is locked, you cannot push to it" );
 		}
-    require 'pusher.php';
+		require 'pusher.php';
 
-    $reflectionMethod = new ReflectionMethod('WP_Deploy_Flow_Pusher', $command_name);
-    $commands = $reflectionMethod->invoke(new WP_Deploy_Flow_Pusher($this->params));
+		$reflectionMethod = new ReflectionMethod('WP_Deploy_Flow_Pusher', $command_name);
+		$commands = $reflectionMethod->invoke(new WP_Deploy_Flow_Pusher($this->params));
 
-    $this->_execute_commands($commands, $args);
+		$this->_execute_commands($commands, $args);
 	}
 
-  protected function _pull_command($command_name, $args, $flags)
-  {
-    $this->params = self::_prepare_and_extract( $args );
-    $this->flags = $flags;
-    extract($this->params);
+	protected function _pull_command($command_name, $args, $flags)
+	{
+		$this->params = self::_prepare_and_extract( $args );
+		$this->flags = $flags;
+		extract($this->params);
 
 		$const = strtoupper( ENVIRONMENT ) . '_LOCKED';
 		if ( constant( $const ) === true ) {
 			return WP_CLI::error( ENVIRONMENT . ' env is locked, you can not pull to your local copy' );
 		}
 
-    require 'puller.php';
+		require 'puller.php';
 
-    $reflectionMethod = new ReflectionMethod('WP_Deploy_Flow_Puller', $command_name);
-    $commands = $reflectionMethod->invoke(new WP_Deploy_Flow_Puller($this->params));
+		$reflectionMethod = new ReflectionMethod('WP_Deploy_Flow_Puller', $command_name);
+		$commands = $reflectionMethod->invoke(new WP_Deploy_Flow_Puller($this->params));
 
-    $this->_execute_commands($commands, $args);
+		$this->_execute_commands($commands, $args);
 	}
 
-  protected function _execute_commands($commands)
-  {
-    if($this->flags['dry-run']) {
-      WP_CLI::line('DRY RUN....');
-    }
+	protected function _execute_commands($commands)
+	{
+		if($this->flags['dry-run']) {
+			WP_CLI::line('DRY RUN....');
+		}
 
 		foreach ( $commands as $command_info ) {
 			list( $command, $exit_on_error ) = $command_info;
-      WP_CLI::line( $command );
-      if(!$this->flags['dry-run']) WP_CLI::launch( $command, $exit_on_error );
+			WP_CLI::line( $command );
+			if(!$this->flags['dry-run']) WP_CLI::launch( $command, $exit_on_error );
 		}
-  }
+	}
 
 	protected static function _prepare_and_extract( $args ) {
 		$out = array();
@@ -111,7 +112,7 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 		$out['db_host'] = escapeshellarg( $out['db_host'] );
 		$out['db_password'] = escapeshellarg( $out['db_password'] );
 		$out['ssh_port'] = ( isset($out['ssh_port']) ) ? intval( $out['ssh_port']) : 22;
-    $out['excludes'] = explode(':', $out['excludes']);
+		$out['excludes'] = explode(':', $out['excludes']);
 		return $out;
 	}
 
@@ -164,6 +165,6 @@ class WP_Deploy_Flow_Command extends WP_CLI_Command {
 		WP_CLI::line( <<<EOB
 
 EOB
-  );
-  }
+	);
+	}
 }
--- lib/puller.php
+++ lib/puller.php
@@ -2,105 +2,105 @@
 
 class WP_Deploy_Flow_Puller {
 
-  public function __construct($params)
-  {
-    $this->params = $params;
-  }
+	public function __construct($params)
+	{
+		$this->params = $params;
+	}
 
-  public function commands()
-  {
-    $commands = array();
+	public function commands()
+	{
+		$commands = array();
 
-    if($this->params['ssh_db_host']) {
-      $this->_commands_for_database_import_thru_ssh($commands);
-    } else {
-      $this->_commands_for_database_import_locally($commands);
-    }
-    $this->_commands_for_database_dump($commands);
+		if($this->params['ssh_db_host']) {
+			$this->_commands_for_database_import_thru_ssh($commands);
+		} else {
+			$this->_commands_for_database_import_locally($commands);
+		}
+		$this->_commands_for_database_dump($commands);
 
-    $commands[]= array('rm dump.sql', true);
+		$commands[]= array('rm dump.sql', true);
 
-    $this->_commands_for_files( $commands );
-    return $commands;
-  }
+		$this->_commands_for_files( $commands );
+		return $commands;
+	}
 
-  public function commands_for_files() {
-    $commands = array();
-    $this->_commands_for_files($commands);
-    return $commands;
-  }
+	public function commands_for_files() {
+		$commands = array();
+		$this->_commands_for_files($commands);
+		return $commands;
+	}
 
-  protected function _commands_for_files(&$commands) {
+	protected function _commands_for_files(&$commands) {
 		extract( $this->params );
 
-    $dir = wp_upload_dir();
-    $dist_path  = constant( WP_Deploy_Flow_Command::config_constant( 'path' ) ) . '/';
-    $remote_path = $dist_path;
-    $local_path = ABSPATH;
+		$dir = wp_upload_dir();
+		$dist_path  = constant( WP_Deploy_Flow_Command::config_constant( 'path' ) ) . '/';
+		$remote_path = $dist_path;
+		$local_path = ABSPATH;
 
-    $excludes = array_merge(
-      $excludes,
-      array(
-        '.git',
-        '.sass-cache',
-        'wp-content/cache',
-        'wp-content/_wpremote_backups',
-        'wp-config.php',
-      )
-    );
+		$excludes = array_merge(
+			$excludes,
+			array(
+				'.git',
+				'.sass-cache',
+				'wp-content/cache',
+				'wp-content/_wpremote_backups',
+				'wp-config.php',
+			)
+		);
 
-    if(!$ssh_host) {
-       // in case the source env is in a subfolder of the destination env, we exclude the relative path to the source to avoid infinite loop
-      $remote_local_path = realpath($local_path);
-      if($remote_local_path) {
-        $remote_path = realpath($remote_path);
-        $remote_local_path = str_replace($remote_path . '/', '', $remote_local_path);
-        $excludes[]= $remote_locale_path;
-      }
-    }
-    $excludes = array_reduce( $excludes, function($acc, $value) { $acc.= "--exclude \"$value\" "; return $acc; } );
+		if(!$ssh_host) {
+			// in case the source env is in a subfolder of the destination env, we exclude the relative path to the source to avoid infinite loop
+			$remote_local_path = realpath($local_path);
+			if($remote_local_path) {
+				$remote_path = realpath($remote_path);
+				$remote_local_path = str_replace($remote_path . '/', '', $remote_local_path);
+				$excludes[]= $remote_locale_path;
+			}
+		}
+		$excludes = array_reduce( $excludes, function($acc, $value) { $acc.= "--exclude \"$value\" "; return $acc; } );
 
-    if ( $ssh_host ) {
-      $commands[]= array("rsync -avz -e 'ssh -p $ssh_port' $ssh_user@$ssh_host:$remote_path $local_path $excludes", true);
-    } else {
-      $commands[]= array("rsync -avz $remote_path $local_path $excludes", true);
-    }
-  }
+		if ( $ssh_host ) {
+			$commands[]= array("rsync -avz -e 'ssh -p $ssh_port' $ssh_user@$ssh_host:$remote_path $local_path $excludes", true);
+		} else {
+			$commands[]= array("rsync -avz $remote_path $local_path $excludes", true);
+		}
+	}
 
-  protected function _commands_for_database_import_thru_ssh(&$commands)
-  {
+	protected function _commands_for_database_import_thru_ssh(&$commands)
+	{
 		extract( $this->params );
-    $host = $db_host . ':' . $db_port;
+		$host = $db_host . ':' . $db_port;
 
-    $dist_path  = constant( WP_Deploy_Flow_Command::config_constant( 'path' ) ) . '/';
-    $commands[]= array("ssh $ssh_user@$ssh_host -p $ssh_port \"cd $dist_path;wp db export dump.sql;\"", true);
-    $commands[]= array("scp $ssh_user@$ssh_host:$dist_path/dump.sql .", true);
-    $commands[]= array("ssh $ssh_user@$ssh_host -p $ssh_port \"cd $dist_path; rm dump.sql;\"", true);
-  }
+		$dist_path  = constant( WP_Deploy_Flow_Command::config_constant( 'path' ) ) . '/';
+		$commands[]= array("ssh $ssh_user@$ssh_host -p $ssh_port \"cd $dist_path;wp db export dump.sql;\"", true);
+		$commands[]= array("scp $ssh_user@$ssh_host:$dist_path/dump.sql .", true);
+		$commands[]= array("ssh $ssh_user@$ssh_host -p $ssh_port \"cd $dist_path; rm dump.sql;\"", true);
+	}
 
-  protected function _commands_for_database_import_locally(&$commands)
-  {
+	protected function _commands_for_database_import_locally(&$commands)
+	{
 		extract( $this->params );
 
-    $host = $db_host . ':' . $db_port;
-    $wpdb = new wpdb( $db_user, $db_password, $db_name, $host );
-    $path = ABSPATH;
-    $url = get_bloginfo( 'url' );
-    $dist_path  = constant( WP_Deploy_Flow_Command::config_constant( 'path' ) ) . '/';
-    $commands[]= array("wp migrate to $path $url dump.sql", true);
-  }
+		$host = $db_host . ':' . $db_port;
+		$wpdb = new wpdb( $db_user, $db_password, $db_name, $host );
+		$path = ABSPATH;
+		$url = get_bloginfo( 'url' );
+		$dist_path  = constant( WP_Deploy_Flow_Command::config_constant( 'path' ) ) . '/';
+		$commands[]= array("wp migrate to $path $url dump.sql", true);
+	}
 
-  protected function _commands_for_database_dump(&$commands) {
+	protected function _commands_for_database_dump(&$commands) {
 		extract( $this->params );
-    $commands[]= array('wp db export db_bk.sql', true);
+		$commands[]= array('wp db export db_bk.sql', true);
 
-    $commands[]= array('wp db import dump.sql', true);
+		$commands[]= array('wp db import dump.sql', true);
 
-    $siteurl = get_option( 'siteurl' );
-    $searchreplaces = array($url => $siteurl, untrailingslashit( $path ) => untrailingslashit( ABSPATH ));
+		$siteurl = get_option( 'siteurl' );
+		$searchreplaces = array($url => $siteurl, untrailingslashit( $path ) => untrailingslashit( ABSPATH ));
 
-    foreach($searchreplaces as $search => $replace) {
-      $commands[]= array( "wp search-replace $search $replace", true );
-    }
-  }
+		foreach($searchreplaces as $search => $replace) {
+			$commands[]= array( "wp search-replace $search $replace", true );
+		}
+	}
 }
